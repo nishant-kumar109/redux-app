cf7d15a953e9468a452598c8ca6d5fb3f1081754
Nishant <nishantindia18@gmail.com>
2020-05-27 17:36:56 +0530

    redux practice basics

---
+++ src/store/reducers/counter.js
@@ -0,0 +1,40 @@
+
+import *as actionTypes from '../actions'
+
+const initialState = {
+    counter:0
+}
+
+const reducer = (state = initialState, action)=>{
+    switch(action.type){
+        case actionTypes.INCREMENT:
+            
+                const newState =  Object.assign({}, state);
+                newState.counter = state.counter+1;
+                return newState;
+            
+        
+        case actionTypes.DECREMENT:{
+            return {
+                ...state,
+                counter : state.counter-1
+            }
+        }
+        case actionTypes.ADD:{
+            return {
+                ...state,
+                counter : state.counter+10
+            }
+        }
+        case actionTypes.SUBTRACT:{
+            return {
+                ...state,
+                counter : state.counter-5
+            }
+        }
+    }
+    return state;
+}
+
+
+export default reducer;
